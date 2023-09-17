5bf76f348c5b9c7168c823e1453eddbc3d7ebfaa
Anshul <142924017+ddemomantf2@users.noreply.github.com>
2023-09-17 22:55:54 +0530

    17-09-23

--- client/src/components/Loading.js
+++ client/src/components/Loading.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Loading() {
     return (
-        <div>
+        <div className='text-center'>
             <div className="spinner-border" role="status" style={{height:'80px' , width:'80px'}}>
             </div>
         </div>
--- client/src/screens/Registerscreen.js
+++ client/src/screens/Registerscreen.js
@@ -1,17 +1,19 @@
-import React, { useState, useEffect } from 'react'
-import {useDispatch , useSelector} from 'react-redux'
-import { registerUser } from '../actions/userActions'
-import Success from '../components/Success'
-import Loading from '../components/Loading'
-import Error from '../components/Error'
+import React, { useState, useEffect } from 'react';
+import {useDispatch , useSelector } from 'react-redux';
+import { registerUser } from '../actions/userActions';
+import Success from '../components/Success';
+import Loading from '../components/Loading';
+import Error from '../components/Error';
 
 export default function Registerscreen() {
     const [name, setname] = useState('');
     const [email, setemail] = useState('');
     const [password, setpassword] = useState('');
     const [cpassword, setcpassword] = useState('');
-    const registerstate = useState(state=>state.registerUserReducer)
+ //uncomment below 2 if workaround is found from vid3 1"07
 
+      
+    const registerstate = useSelector(state=>state.registerUserReducer)
     const {error , loading , success} = registerstate
 
     const dispatch = useDispatch()
@@ -35,11 +37,10 @@ export default function Registerscreen() {
         <div>
             <div className='row justify-content-center mt-5'>
                 <div className='col-md-5 mt-5 text-left shadow-lg p-3 mb-5 bg-white rounded'>
-
-                    {loading && (<Loading/>)}
-                    {success && (<Success success='User Registered Successfully'/>)}
-                    {error && (<Error error='Email already registered'/>)}
-
+                {loading && (<Loading/>)}
+                {success && (<Success success='User Registered Successfully'/>)}
+                {error && (<Error error='Email already registered'/>)}
+                    
 
                     <h2 className='text-center m-2' style={{ fontSize: '35px' }}>Register</h2>
                     <div>
@@ -58,3 +59,8 @@ export default function Registerscreen() {
         </div>
     )
 }
+
+/*
+put the code below in line 38
+
+*/
